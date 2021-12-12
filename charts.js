3231feed60376bf2fb2cd99dd9411b2db878658a
Manuel Andres Ruiz Faller <ruizfaller@hotmail,com>
2021-12-12 16:54:10 -0600

    m

--- charts.js
+++ charts.js
@@ -56,32 +56,41 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {    
+    
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
-
+    var result = resultArray[0];
+    //var PANEL = d3.select("#sample-metadata");
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    let ids = result.otu_ids;
+    let labels = result.otu_labels;
+    let values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    //var yticks = 
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var trace = {
+      x: sample_values,
+      y: otu_labels,
+      type: "bar"
+    };
+    var barData = [trace];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     
+    var barLayout = {  
+      title: "Top 10 Bacteria Cultures Found",
+      //xaxis: {title: "City" },
+      //yaxis: {title: "Population Growth, 2016-2017"}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
+
   });
 }
